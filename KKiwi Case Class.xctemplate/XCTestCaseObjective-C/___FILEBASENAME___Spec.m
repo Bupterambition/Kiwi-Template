b1bf7947d5b8344ba924e60cca5df1d6de0e263a
senmiao <senmiao@mogujie.com>
2016-07-31 13:35:47 +0800

    First Init

---
+++ KKiwi Case Class.xctemplate/XCTestCaseObjective-C/___FILEBASENAME___Spec.m
@@ -0,0 +1,44 @@
+//
+//  ___FILEBASENAMEASIDENTIFIER___Spec.m
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import <Kiwi/Kiwi.h>
+#import <Foundation/Foundation.h>
+#import "___VARIABLE_cocoaTouchSubclass___.h"
+
+SPEC_BEGIN(___FILEBASENAMEASIDENTIFIER___Spec)
+
+describe(@"Example", ^{
+    
+    beforeAll(^{
+    
+        // Put setup code here. This method is called before the invocation of All Block in the class.
+    });
+    
+    
+    context(@"when ", ^{
+        
+        beforeEach(^{
+            
+            // Put setup code here. This method is called before the invocation of each test method in the class.
+        });
+        
+        afterEach(^{
+            
+            // Put teardown code here. This method is called after the invocation of each test method in the class.
+        });
+        
+        it(@"should ", ^{
+            
+            // This is an example of a functional test case.
+        });
+        
+    });
+    
+});
+
+SPEC_END
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Kiwi-Template
+
+[![CI Status](http://img.shields.io/travis/senmiao/Kiwi-Template.svg?style=flat)](https://travis-ci.org/senmiao/MGJUnitTest)
+[![Version](https://img.shields.io/cocoapods/v/Kiwi-Template.svg?style=flat)](http://cocoapods.org/pods/MGJUnitTest)
+[![License](https://img.shields.io/cocoapods/l/Kiwi-Template?style=flat)](http://cocoapods.org/pods/MGJUnitTest)
+[![Platform](https://img.shields.io/cocoapods/p/Kiwi-Template.svg?style=flat)](http://cocoapods.org/pods/MGJUnitTest)
+
+## Example
+
+用于生成Kiwi测试Case的模版，可以在构建时选择要测试的类 
+
+## Installation
+
+打开终端输入sudo sh install-templates.sh
+
+```
+sudo chmod 755 install-templates.sh
+
+sudo sh install-templates.sh 
+```
+运行完毕后会显示
+
+```
+Finished
+
+```
+## Author
+
+senmiao, senmiao@mogujie.com
+
+## License
+
+MGJUnitTest is available under the MIT license. See the LICENSE file for more info.
