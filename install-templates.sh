52f6d62342ab789e683e7e7a4bf1752793296e89
senmiao <senmiao@mogujie.com>
2016-10-28 01:10:31 +0800

    support Xcode8

--- install-templates.sh
+++ install-templates.sh
@@ -5,7 +5,7 @@ TEMPLATES_DIR="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.pla
 FILE_TEMPLATES_DIR="$TEMPLATES_DIR/Templates/File Templates"
 Kiwi_TEMPLATES_DIR="$FILE_TEMPLATES_DIR/Source"
 SOURCE_Kiwi_DIR="$SOURCE_DIR"
-SOURCE_DIR="$SOURCE_Kiwi_DIR/KKiwi Case Class.xctemplate"
+SOURCE_DIR="$SOURCE_Kiwi_DIR/Kiwi Case Class.xctemplate"
 
 echo "Installing templates to $Kiwi_TEMPLATES_DIR from $SOURCE_DIR"
 cp -R "$SOURCE_DIR" "$Kiwi_TEMPLATES_DIR"
